77610745be9ca96454161bca52b9111ae2716f7e
andreiserban123 <andyspeed2003@gmail.com>
2022-11-28 19:25:16 +0200

    seminar matrice

--- serminar/24.11.2022/pb5.c
+++ serminar/24.11.2022/pb5.c
@@ -1,1 +1,26 @@
 // cerinta : transpusa unei matrice dreptunghice
+
+#include <stdio.h>
+
+int main()
+{
+    float mat[100][100], t[100][100];
+    int m, n;
+    scanf("%d %d", &m, &n);
+    for (int i = 0; i < m; i++)
+        for (int j = 0; j < n; j++)
+            scanf("%f", &mat[i][j]);
+    for (int i = 0; i < n; i++)
+        for (int j = 0; j < m; j++)
+        {
+            t[i][j] = mat[j][i];
+        }
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < m; j++)
+            printf("%.2f\t", t[i][j]);
+        printf("\n");
+    }
+
+    return 0;
+}
--- serminar/24.11.2022/pb6.c
+++ serminar/24.11.2022/pb6.c
@@ -1,3 +1,38 @@
 // cerinta : fie informatiile referitoare la m studenti si n discipline
 // a) identificati studentii integralisti toate notele >=5
-// b)identificati studentii bursieri >= 8,5 si integralisti
+// b)identificati studentii bursieri >= 8,5 si (+)integralisti
+
+#include <stdio.h>
+
+int main()
+{
+    float mat[100][100];
+    int m, n, in, bu;
+    in = 0;
+    bu = 0;
+    scanf("%d %d", &m, &n);
+    for (int i = 0; i < m; i++)
+        for (int j = 0; j < n; j++)
+            scanf("%f", &mat[i][j]);
+    for (int i = 0; i < m; i++)
+    {
+        float s = 0;
+        int ok = 1;
+
+        for (int j = 0; j < n; j++)
+        {
+            if (mat[i][j] < 5)
+            {
+                ok = 0;
+            }
+            s += mat[i][j];
+        }
+        if (ok == 1)
+            in++;
+        if (ok == 1 && (s / n) > 8.5)
+            bu++;
+    }
+    printf("bursieri sunt: %d\n", bu);
+    printf("integralisti sunt: %d\n", in);
+    return 0;
+}
